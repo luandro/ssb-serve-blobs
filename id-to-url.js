306db8e0f1de598c2d84cc079aea255b9ba56683
luandro <luandro@gmail.com>
2019-10-20 11:13:16 -0300

    changed localhost to machine ip

--- id-to-url.js
+++ id-to-url.js
@@ -1,5 +1,7 @@
-const PORT = require('./port');
-module.exports = function idToUrl(blobId) {
-  const blobRef = encodeURIComponent(blobId);
-  return `http://localhost:${PORT}/${blobRef}`;
+const ip = require('ip')
+const PORT = require('./port')
+module.exports = function idToUrl (blobId) {
+  const myIp = ip.address()
+  const blobRef = encodeURIComponent(blobId)
+  return `http://${myIp || 'localhost'}:${PORT}/${blobRef}`
 }
--- url-to-id.js
+++ url-to-id.js
@@ -1,5 +1,7 @@
-const PORT = require('./port');
-module.exports = function urlToId(url) {
-  const blobRef = url.split(`http://localhost:${PORT}/`)[1];
-  return decodeURIComponent(blobRef);
+const ip = require('ip')
+const PORT = require('./port')
+module.exports = function urlToId (url) {
+  const myIp = ip.address()
+  const blobRef = url.split(`http://${myIp || 'localhost'}:${PORT}/`)[1]
+  return decodeURIComponent(blobRef)
 }
